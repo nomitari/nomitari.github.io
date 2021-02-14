3f4d700e52b79cf0c1e1b180ff037da396c4b1c4
Nomi <nomi@nomis-computer.local>
2021-02-14 16:24:27 -0700

    Update files

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 function hamburger() 
 {
 	var x = document.getElementById("myLinks");
@@ -11,8 +12,6 @@ function hamburger()
 	}
 }
 
-window.onresize = resizeHamburger;
-
 function resizeHamburger() 
 {
 	var x = screen.width;
@@ -23,47 +22,5 @@ function resizeHamburger()
 	}
 }
 
-var x = "Total Width: " + screen.width;
-
-var modal = document.getElementById("modal");
-var page = document.getElementById("contact-body");
-
-// Get the button that opens the modal
-var btn = document.getElementById("email-button");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-// btn.onclick = function() {
-// 	modal.style.display = "block";
-// }
-
-// $("#email-button").click(function(){
-// 	$('#contact-page').css("filter","blur(3px)");
-// });
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() 
-{
-	modal.style.display = "none";
-}
-
-$("#close-btn").click(function()
-{
-	$('#contact-page').css("filter","");
-});
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) 
-{
-	if (event.target == modal) 
-	{
-		modal.style.display = "none";
-  	}
-}
-
-
-
-
+window.onresize = resizeHamburger;
 
